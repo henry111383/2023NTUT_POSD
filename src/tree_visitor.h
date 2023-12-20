ac528835376694f5bae3fd4f69d7a43947c6a28e
JingHan <63907091+henry111383@users.noreply.github.com>
2023-12-20 16:06:34 +0000

    HW6

--- src/tree_visitor.h
+++ src/tree_visitor.h
@@ -2,10 +2,27 @@
 
 #include "visitor.h"
 #include "order_by.h"
+#include "iterator_factories.h"
 
 class TreeVisitor: public Visitor {
 public:
-    TreeVisitor(OrderBy orderBy): _orderBy(orderBy), _currentLevel(0), _notEndLevel(0) {}
+    TreeVisitor(OrderBy orderBy): _orderBy(orderBy), _currentLevel(0), _notEndLevel(0) {
+        if(_orderBy == OrderBy::Normal){
+            _factory = new FolderIteratorFactory();
+        }
+        else if (_orderBy == OrderBy::Name)
+        {
+            _factory = new OrderByNameIteratorFactory();
+        }
+        else if (_orderBy == OrderBy::NameWithFolderFirst)
+        {
+            _factory = new OrderByNameWithFolderFirstIteratorFactory();
+        }
+        else{
+            _factory = new OrderByKindIteratorFactory();
+        }
+    }
+    TreeVisitor(IteratorFactory *factory): _factory(factory){}
 
     void visitFile(File * file) {
         _result += file->name() + "\n";
@@ -17,7 +34,8 @@ public:
             _result += folder->name() + "\n";
         }
 
-        Iterator * it = folder->createIterator(_orderBy);
+        // Iterator * it = folder->createIterator(_orderBy);
+        Iterator * it = folder->createIterator(_factory);
         it->first();
         while (!it->isDone()) {
             Node * current = it->currentItem();
@@ -59,4 +77,5 @@ private:
     std::string _result;
     int _currentLevel;
     int _notEndLevel;
+    IteratorFactory *_factory;
 };
